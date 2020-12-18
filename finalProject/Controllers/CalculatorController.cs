5b4524a3170885ded183b8f6933e9406a8ba9e56
Koppisetti satya kumar <satyakoppisetti95@gmail.com>
2020-12-18 00:32:06 -0500

    #1 - Create a basic Calculator Application

---
+++ finalProject/Controllers/CalculatorController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using finalProject.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace finalProject.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ExpressionModel exp)
+        {
+            ViewBag.operand1 = exp.operand1;
+            ViewBag.operand2 = exp.operand2;
+            ViewBag.operator_symbol = exp.operator_symbol;
+
+            switch (exp.operator_symbol)
+            {
+                case "+":
+                    ViewBag.result = (exp.operand1 + exp.operand2).ToString("0.####");
+                    break;
+                case "-":
+                    ViewBag.result = (exp.operand1 - exp.operand2).ToString("0.####");
+                    break;
+                case "*":
+                    ViewBag.result = (exp.operand1 * exp.operand2).ToString("0.####");
+                    break;
+                case "/":
+                    if (exp.operand2 != 0)
+                    {
+                        ViewBag.result = (exp.operand1 / exp.operand2).ToString("0.#######");
+                    }
+                    else
+                    {
+                        ViewBag.result = "Error : Can't Divide by zero";
+                    }
+                    break;
+                default:
+                    ViewBag.result = (exp.operand1 + exp.operand2).ToString("0.####");
+                    break;
+            }
+            return View();
+        }
+    }
+}
---
+++ finalProject/Models/ExpressionModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace finalProject.Models
+{
+    public class ExpressionModel
+    {
+        public double operand1 { get; set; }
+
+        public double operand2 { get; set; }
+
+        public string operator_symbol { get; set; }  //named operator_symbol as operator seems to be keyword
+
+    }
+}
